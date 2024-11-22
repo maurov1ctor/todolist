91d56ec56b78b4a71e2f5129e6a84c335dcc52fd
maurov1ctor <maurocarvalho745@gmail.com>
2024-11-22 11:13:07 -0300

    first commit

---
+++ src/test/java/br/com/api/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
